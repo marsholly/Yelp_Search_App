2e160b414375de444b3c4eae168d1c84b858e3b2
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-11 00:00:28 -0700

    api success

--- src/components/YelpBoard.js
+++ src/components/YelpBoard.js
@@ -1,7 +1,6 @@
 import  React, { Component } from 'react';
 import YelpStore from '../stores/YelpStore';
 
-
 export default class YelpBoard extends Component {
   constructor() {
     super();
@@ -25,14 +24,43 @@ export default class YelpBoard extends Component {
 
   render() {
     let { infos } = this.state;
+    let infoDiv;
     if (infos) {
       let { businesses } = infos;
-      let {name, rating_img_url}
-      console.log('businesses:', businesses);
+      if( businesses ) {
+        infoDiv = businesses.map((business, index) => {
+          let { name, rating_img_url, location, image_url} = business;
+          let {city, display_address} = location;
+          let address = display_address.map( a => {
+            return <p key = {a}>{a}</p>
+          })
+          return (
+            <div className="col-md-3" key={index}>
+              <div className="infoArea">
+                <div>
+                  <img src={image_url} height="100" width="160"/>
+                </div>
+                <hr/>
+                <div>
+                  <p>{name}</p>
+                  <div>
+
+                  </div>
+                  <p><img src={rating_img_url}/></p>
+                  <p>{city}</p>
+                  {address}
+                </div>
+              </div>
+            </div>
+          )
+        })
+      } else {
+        infoDiv = <div></div>
+      }
     }
     return (
       <div className="row">
-
+        {infoDiv}
       </div>
     )
   }
